eed325d07fda965b6c27e38c5f98dc3af7c3ab7d
Max <jmlong.dev@gmail.com>
2022-06-27 19:00:55 -0400

    added header, button  and cart svg

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img alt="meals" src={mealsImage} />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
