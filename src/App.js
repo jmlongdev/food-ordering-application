e1a2481b223b20fc759f6c5cfcaf8f1e5509b177
Max <jmlong.dev@gmail.com>
2022-06-27 19:33:54 -0400

    meals added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Header from "./components/Layout/Header";
+import MealsSummary from "./components/Meals/MealsSummary";
 
 function App() {
   return (
     <React.Fragment>
       <h2>Let's get started!</h2>
       <Header />
+      <MealsSummary />
     </React.Fragment>
   );
 }
