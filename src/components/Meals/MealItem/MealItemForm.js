7999f117fbc84b5cf0c3ca64f9e9341aa28e2a03
Max <jmlong.dev@gmail.com>
2022-06-30 18:50:35 -0400

    added modal

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -5,7 +5,17 @@ import classes from "./MealItemForm.module.css";
 const MealItemForm = (props) => {
   return (
     <form className={classes.form}>
-      <Input />
+      <Input
+        label="amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>+ Add</button>
     </form>
   );
