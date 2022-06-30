15656f1a7154d84248515abca1bd5cebd890c4a3
Max <jmlong.dev@gmail.com>
2022-06-29 23:30:07 -0400

    added input componenet

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,6 +1,9 @@
 import React from "react";
 
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
+
 const DUMMY_MEALS = [
   {
     id: "m1",
@@ -30,11 +33,20 @@ const DUMMY_MEALS = [
 
 // No need for props either
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meal}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <Input />
+      <button>+ Add</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
